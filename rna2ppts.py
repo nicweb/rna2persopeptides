46ded949b12e95398e846c39db6ce082c8bb2286
Nico <nico.weber@uni-tuebingen.de>
2018-07-26 12:38:40 +0200

    restructure for convert func

--- rna2ppts.py
+++ rna2ppts.py
@@ -2,6 +2,8 @@ import argparse
 import subprocess
 import time
 
+import rna2ppts
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-r1", help="First RNA-FastQFile")
 parser.add_argument("-r2", help="Second RNA-FastQFile")
@@ -17,122 +19,7 @@ args=vars(parser.parse_args())
 
 star_exec="STAR"
 spladder_exec="/software/spladder/python/spladder.py"
-def run_external_cmd(command):
-	#bad error handling at the moment
-	#infos only printed when process finishes
-	print "Executing Command %s" %' '.join(command)
-	t0=time.time()
-	try:
-		subprocess.check_output(command, stderr=subprocess.STDOUT)
-	except subprocess.CalledProcessError as e:
-		print "ERROR executing Job"
-		print e.output
-		exit(1)
-	t1=time.time()
-	print "Execution took %i seconds"%(t1-t0)
 
-def run_createindex(args):
-#create directory and create star index
-	if not (args['starindex'] and args['annotation'] and args['fasta']):
-		print "not enough arguments for creating"
-		exit(1)
-	print "creating directory"
-	subprocess.check_output(["mkdir",args['starindex']])
-	print "Creating Star Index"
-	starcreateindex_cmd=[star_exec, \
-					"--runMode", "genomeGenerate", \
-					"--runThreadN", "4", \
-					"--genomeDir", args['starindex'], \
-					"--genomeFastaFiles",args['fasta'], \
-					"--sjdbGTFfile",args['annotation'], \
-					"--sjdbGTFtagExonParentTranscript","Parent", \
-					"-sjdbOverhang","100"]
-	run_external_cmd(starcreateindex_cmd)
-
-
-if (args['createindex']):
-	print "calling creating index subroutine"
-	run_createindex(args)
-
-
-def run_star1stpass(args):
-	if not (args['starindex'] and args['r1'] and args['r2'] and args['fasta'] and args['outputdir']):
-		print "not enough arguments for 1. star run"
-		exit(1)
-	star1stpass_cmd=[star_exec, \
-           "--genomeDir",args['starindex'], \
-           "--readFilesIn",args['r1'],args['r2'], \
-           "--runThreadN","4", \
-           "--outFilterMultimapScoreRange","1", \
-           "--outFilterMultimapNmax","20", \
-           "--outFilterMismatchNmax","10", \
-           "--alignIntronMax","500000", \
-           "--alignMatesGapMax","1000000", \
-           "--sjdbScore","2", \
-           "--alignSJDBoverhangMin","1", \
-           "--genomeLoad","NoSharedMemory", \
-           "--readFilesCommand","cat", \
-           "--outFilterMatchNminOverLread","0.33", \
-           "--outFilterScoreMinOverLread","0.33", \
-           "--sjdbOverhang","100", \
-           "--outSAMstrandField","intronMotif", \
-           "--outSAMtype","None", \
-           "--outSAMmode","None"]#, \
-       #    "--outFileNamePrefix",args['outputdir']]
-	print "calling 1.pass Star Mapping subroutine"
-	run_external_cmd(star1stpass_cmd)
-		   
-def run_star2ndpass(args):
-	star2ndpass_cmd=[star_exec, \
-             "--genomeDir","GENOME_TMP", \
-             "--readFilesIn",args['r1'],args['r2'], \
-             "--runThreadN","4", \
-             "--outFilterMultimapScoreRange","1", \
-             "--outFilterMultimapNmax","20", \
-             "--outFilterMismatchNmax","10", \
-             "--alignIntronMax","500000", \
-             "--alignMatesGapMax","1000000", \
-             "--sjdbScore","2", \
-             "--alignSJDBoverhangMin","1", \
-             "--genomeLoad","NoSharedMemory", \
-             "--limitBAMsortRAM","70000000000", \
-             "--readFilesCommand","cat", \
-             "--outFilterMatchNminOverLread","0.33", \
-             "--outFilterScoreMinOverLread","0.33", \
-             "--sjdbOverhang","100", \
-             "--outSAMstrandField","intronMotif", \
-             "--outSAMattributes","NH","HI","NM","MD","AS","XS", \
-             "--outSAMunmapped","Within", \
-             "--outSAMtype","BAM","SortedByCoordinate", \
-             "--outSAMheaderHD","@HD VN:1.4", \
-             "--outSAMattrRGline","ID:SM"]#, \
-           #  "--outFileNamePrefix",args['outputdir']]
-	print "calling 2. pass star mapping subroutine"
-	run_external_cmd(star2ndpass_cmd)
-
-def run_starreindex(args):
-	#TODO: Add annotation???
-	print "creating directory"
-	subprocess.check_output(["mkdir","GENOME_TMP"])
-	starreindex_cmd=[star_exec, \
-             "--runMode","genomeGenerate", \
-             "--genomeDir","GENOME_TMP", \
-             "--genomeFastaFiles",args['fasta'], \
-             "--sjdbOverhang","100", \
-             "--runThreadN","4", \
-             "--sjdbFileChrStartEnd","SJ.out.tab"]#, \
-         #    "--outFileNamePrefix",args['outputdir']]
-	print "calling star redindexing"
-	run_external_cmd(starreindex_cmd)
-	
-def run_spladder(args):
-	run_spladder_cmd=["python", \
-              spladder_exec, \
-              "-b","Aligned.sortedByCoord.out.bam", \
-              "-o","spladdrout", \
-              "-a",args['annotation']]
-	print "calling spladder subroutine"
-	run_external_cmd(run_spladder_cmd)
 	
 t_start=time.time()			  
 #run_star1stpass(args)
---
+++ rna2ppts/converter.py
@@ -0,0 +1,171 @@
+from BCBio import GFF
+import operator
+from Bio.SeqRecord import SeqRecord
+import glob, os
+from Bio import SeqIO
+import time
+import pysam
+
+import gffutils
+import pyfaidx
+from pysam import VariantFile
+from Bio.Seq import Seq
+
+sourcebase= '/mnt/test_data/hepavac34/rna_benign/spladdrout'
+#sourcebase = '/tmp/'
+reffile = '/mnt/test_data/refs/GRCh37.primary_assembly.genome.fa'
+fileending = 'gff3'
+vcffile= '/tmp/vcf.vcf.gz' #must be indexed!
+
+#db = gffutils.create_db('/tmp/ensg228794.gff', ':memory:')
+fasta = pyfaidx.Fasta('/mnt/test_data/refs/GRCh37.primary_assembly.genome.fa')
+vcffile='/tmp/vcf.vcf.gz'
+
+outfile = '/tmp/predicted_exons.fa'
+outfile2 = '/tmp/predicted_genes.fa'  
+vcfrecords=VariantFile(vcffile)
+
+
+#Getting all gff-files in base directory
+def get_gff_files(sourcebase):
+    print "Checking for GFF Files in directory..."
+    gfffiles = []
+    os.chdir(sourcebase)
+    for file in glob.glob("*.%s" %fileending):
+        gfffiles.append(file)
+    print "Found %i file(s) in %s ending with %s." %(len(gfffiles), sourcebase, fileending)
+    return gfffiles
+
+def get_gene_sequences(gene,db):
+    sequences = []
+    geneseq = []
+    num_mod=0
+    #print gene
+    for i in db.children(gene,featuretype='exon'):
+        events=check_feature_for_vcfevent(i)
+        if len(events) == 0:
+            rec = i.sequence(fasta)#.translate(to_stop=True)
+            sequences.append(SeqRecord(Seq(rec),i.id,i.attributes['Parent'][0],""))
+            geneseq.append(rec)
+        else:
+            num_mod+=1
+        #    sequences.append(Seq.translate(i.sequence(fasta),to_stop=True))
+            rec = get_modified_sequence(i.sequence(fasta),events,i.start)
+            sequences.append(SeqRecord(Seq(rec),i.id,i.attributes['Parent'][0],""))
+            geneseq.append(rec)
+            #sequences.append(get_modified_sequence(i.sequence(fasta),events,i.start))
+            #return get_modified_sequence(i.sequence(fasta),events)
+        #if i.id!='exon_7569':
+        #    print "this is the record"
+        #    print sequences[-1]
+        
+    return sequences,[SeqRecord(Seq(''.join(geneseq)),gene.id,"")], num_mod 
+
+def check_feature_for_vcfevent(feature):
+    vcfevents = []
+    if feature.chrom not in vcfrecords.header.contigs:
+        print "Chrom not found"
+        return vcfevents
+    for rec in vcfrecords.fetch(feature.chrom,feature.start,feature.stop):
+        vcfevents.append(rec)
+       #a=rec
+    if len(vcfevents) == 0:
+        return vcfevents
+        print "no events in range detected (%s - %i on %i)"%(feature.chrom,feature.start,feature.stop)
+    else:
+        print "%i events in range detected (%s - %i on %i)"%(len(vcfevents),feature.chrom,feature.start,feature.stop)
+        
+    return vcfevents
+
+def get_modified_sequence(in_seq,events,offset):
+    print "Personalisizing sequence len:%i with %i events"%(len(in_seq),len(events))
+    lengthchange=0
+    for event in events:
+        eventpos=event.start-offset
+        print "Eventstart %i, offset: %i -> pos:%i"%(event.start,offset,eventpos)
+        print "Char at  %i: %s (region +-3: %s)" %(eventpos,in_seq[eventpos],in_seq[eventpos-3:eventpos+3])
+        alleles=event.alleles
+        l = list(in_seq)
+        
+        if is_snp(alleles):
+          #  print "Found SNP substituting %s with %s" %(l[eventpos],alleles[1])
+            if l[eventpos]!= alleles[0]:
+                print "warning SNP missmatch"
+            l[eventpos]=alleles[1]
+        if is_del(alleles):
+            l[eventpos]=alleles[1] #TODO falsch!
+            lengthchange-=len(alleles[1])-len(alleles[0])
+            print "Found deletion: deleting %s from sequences"%alleles[0]
+            print alleles
+            print event
+        if is_insert(alleles):
+            l.insert(eventpos,alleles[1])
+            lengthchange+=len(alleles[0])-len(alleles[1])
+            print "found insert: inserting %s into sequence at pos %i"%alleles[1],eventpos
+            print event
+    new_seq=''.join(l)
+    if (len(in_seq)-len(new_seq)) != lengthchange:
+        print "Warning: length changed %i (should be %i)" %(len(in_seq)-len(new_seq),lengthchange)
+    return new_seq 
+
+def is_snp(e):
+    if len(e[0]) == 1 and len(e[1]) == 1:
+        return True
+    return False
+
+def is_insert(e):
+    if len(e[0])<len(e[1]):
+        return True
+    return False
+
+def is_del(e):
+    if len(e[0])>len(e[1]):
+        return True
+    return False
+
+def translate_records(records):
+    new_records = []
+    for rec in records:
+        for i in range(0,3):
+            new_records.append(SeqRecord(rec.seq[i:].translate(to_stop=True),"%s %s TL-Windows: %i "%(rec.name,rec.id,i),rec.name,""))
+    return new_records
+
+def run():
+    gfffiles = get_gff_files(sourcebase)
+    
+    for infile in gfffiles:
+        total_genes=0
+        total_exons=0
+        total_genes_wrote=0
+        total_exons_wrote=0
+        total_mod=0
+        db = gffutils.create_db(infile, ':memory:')
+        for gene in db.features_of_type('gene'):
+            total_genes+=1
+            seqs,geneseq,num_mod=get_gene_sequences(gene,db)
+            total_exons+=len(seqs)
+            total_mod+=num_mod
+            #a=seqs
+           # return seqs
+            #return geneseq
+            total_exons_wrote+=write_records(translate_records(seqs),'exons')
+            total_genes_wrote+=write_records(translate_records(geneseq),'genes')
+        print "processed (and wrote): %s with %i(%i) genes and %i(%i) exons (%i modified))" %(infile,total_genes,total_genes_wrote,total_exons,total_exons_wrote,total_mod)
+        
+    #ref_recs = load_reffile(reffile)
+    
+     #   records, records_until_stop = get_records(infile,ref_recs)
+      #  write_records(records,records_until_stop,os.path.basename(infile))
+        
+
+def write_records(records,type):
+    #translate records
+    
+    if type == 'exons':
+        with open(outfile, "a") as out_handle:
+            #print(records[1])
+            return SeqIO.write(records, out_handle, "fasta")
+    if type == 'genes':
+        with open(outfile2, "a") as out_handle:
+            #print(records[1])
+            return SeqIO.write(records, out_handle, "fasta")
